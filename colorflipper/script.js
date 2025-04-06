16da65407c9c9bfda9c32b072b5061d591b83006
Yuriy Tsayukov <yuriy.tsayukov@outlook.com>
2025-04-06 20:27:11 +0200

    add initial JavaScript code and include script in index.html

---
+++ colorflipper/script.js
@@ -0,0 +1,20 @@
+"use strict";
+
+// a = 15;
+// console.log(a);
+
+// let number = 5;
+// const leftBorderWith = 1;
+
+// number = 10;
+// console.log(number);
+
+// const vehicleBodyWidth = 5000;
+// const length = 400;
+
+// console.log(vehicleBodyWidth + length);
+
+let userName = "John";
+let userNumber = 25;
+userNumber = 24;
+console.log(userNumber);
